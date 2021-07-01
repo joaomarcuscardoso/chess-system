beb72d7cdbd36c4459dcf851804f730d23f74cc4
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-07-01 00:19:43 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+package src.boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public boolean setRow(int row) {
+        if (row > 0) {
+            this.row = row;
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+
+    public boolean setColumn(int column) {
+        if (column > 0) {
+            this.column = column;
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+}
